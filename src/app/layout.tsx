c62faf4d948d680f7068c851a253ca6af416ed70
Pradhumn Gupta <pradhumn.115@gmail.com>
2025-07-23 18:37:15 +0530

    added vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { MenuProvider } from "./context/MenuContext";
-
+import { Analytics } from "@vercel/analytics/next"
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -74,6 +74,7 @@ export default function RootLayout({
       >
           <MenuProvider>
             {children}
+            <Analytics />
           </MenuProvider>
       </body>
     </html>
